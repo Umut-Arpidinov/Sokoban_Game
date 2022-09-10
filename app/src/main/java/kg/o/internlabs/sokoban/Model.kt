de9a69133047e7ba90c4cc8b5add4b2ba02d3eb9
Умут Арпидинов <ymytbek0314@gmail.com>
2022-09-10 15:26:35 +0600

    create MVC pattern

---
+++ app/src/main/java/kg/o/internlabs/sokoban/Model.kt
@@ -0,0 +1,13 @@
+package kg.o.internlabs.sokoban
+
+class Model {
+    private val viewer: Viewer
+
+    constructor(viewer: Viewer) {
+        this.viewer = viewer
+        println("I am model object")
+
+
+    }
+
+}
